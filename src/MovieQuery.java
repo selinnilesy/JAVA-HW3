59b27a7fc456b9c597d65b4d6f1145fe65a04413
Selin <e2310639@ceng.metu.edu.tr>
2022-06-07 19:26:56 +0300

    quer5 ok

--- src/MovieQuery.java
+++ src/MovieQuery.java
@@ -6,14 +6,13 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MovieQuery {
        public static void queryAll(String inFileName, String outFileName) {
         List<String> rawData;
         try{
-            Path path = Paths.get(inFileName);
+            Path path = Paths.get(System.getProperty("user.dir") + "/" + inFileName + ".csv");
             rawData = Files.lines(path).collect(Collectors.toList());
         } catch (Exception e) {
             System.out.println("Wrong file path : " + inFileName);
@@ -24,10 +23,11 @@ public class MovieQuery {
         // also transform each line into array for queries to use stream<String[]>.
         rawData.remove(0);
         List<String[]> preparedData = rawData.stream().map(line -> line.split(",")).collect(Collectors.toList());
-        query1(preparedData, outFileName+"-1.out");
-        query2(preparedData, outFileName+"-2.out");
-        query3(preparedData, outFileName+"-3.out");
-        query4(preparedData, outFileName+"-4.out");
+        query1(preparedData,System.getProperty("user.dir") + "/"+ outFileName + "-1.out");
+        query2(preparedData, System.getProperty("user.dir") + "/"+ outFileName + "-2.out");
+        query3(preparedData, System.getProperty("user.dir") + "/"+ outFileName + "-3.out");
+        query4(preparedData, System.getProperty("user.dir") + "/"+ outFileName + "-4.out");
+        query5(preparedData, System.getProperty("user.dir") + "/"+ outFileName + "-5.out");
     }
 
     public static void query1(List<String[]> collection, String outFileName){
@@ -56,17 +56,32 @@ public class MovieQuery {
 
         List<String> succesfull_ones = StreamProcessor.applyLambdaToStream(collection.stream(), MovieQuery::fetchDirectorsWithHighRatings).collect(Collectors.toList());
         List<String> failed_ones =StreamProcessor.applyLambdaToStream(collection.stream(), MovieQuery::fetchDirectorsWithLowRatings).collect(Collectors.toList());
-
-        formatStreamForOutput(succesfull_ones.stream().
-                                            filter(failed_ones::contains))
-                .forEach(outputFile_4::println);
+        // local variables can be used but not modified
+        formatStreamForOutput(succesfull_ones.stream()
+                                            .filter(failed_ones::contains)
+                            ).forEach(outputFile_4::println);
         outputFile_4.close();
     }
+    public static void query5(List<String[]> collection, String outFileName){
+        PrintWriter outputFile_5 =  QueryOutputWriter.processOutput(outFileName, MovieQuery::outputQuery);
+        List<String> director_earliest = collection.stream()
+                                                    .min(Comparator.comparingInt(arr -> Integer.parseInt(arr[1])))
+                                                    .stream()
+                                                    .map(arr -> arr[6]).collect(Collectors.toList());
+        Integer sum = collection.stream()
+                .filter(arr -> arr[6].equals(director_earliest.get(0)))
+                .map(arr -> Integer.parseInt(arr[3]))
+                .reduce(0, Integer::sum);
+        outputFile_5.println(sum);
+
+        outputFile_5.close();
+    }
+
     public static Stream<String> fetchAdventureWithLeastRevenue(Stream<String[]> stream) {
         return stream.filter(arr -> (arr[2].contains("Adventure") &&
                         !Objects.equals(arr[8], "-1")))
                 .distinct()
-                .sorted(Comparator.comparingDouble(MovieQuery::getGross))
+                .sorted(Comparator.comparingDouble(arr -> Double.parseDouble(arr[8])))
                 .findFirst().stream()
                 .map(arr -> arr[6]);
     }
@@ -74,29 +89,26 @@ public class MovieQuery {
         return stream.map( arr -> arr[0]);
     }
     public static Stream<String> fetchDirectorsWithHighRatings(Stream<String[]> stream) {
-        return stream.filter(x-> Double.valueOf(x[4]) >= 8.5
+        return stream.filter(x-> Double.parseDouble(x[4]) >= 8.5
                 )
                 .map( arr -> arr[6])
                 .distinct();
     }
     public static Stream<String> fetchDirectorsWithLowRatings(Stream<String[]> stream) {
-        return stream.filter(x-> Double.valueOf(x[4]) <= 8.0)
+        return stream.filter(x-> Double.parseDouble(x[4]) <= 8.0)
                 .map( arr -> arr[6])
                 .distinct();
     }
     public static PrintWriter outputQuery(String outFileName){
         try{
-            PrintWriter outputFile = new PrintWriter(Files.newBufferedWriter(Paths.get(outFileName)), true);
-            return outputFile;
+            return new PrintWriter(Files.newBufferedWriter(Paths.get(outFileName)), true);
         }
         catch(Exception e){
             System.out.println(e);
             return null;
         }
     }
-    public static double getGross(String[] arr){
-       return Double.parseDouble(arr[8]);
-    }
+
     public static Stream<String> formatStreamForOutput(Stream<String> stream){
         return stream.map(String::toUpperCase)
                 .sorted();
--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 public class main {
     public static void main(String[] args) {
-        MovieQuery.queryAll("/Users/selin/Desktop/oop\shw3/imdb3.csv", "/Users/selin/Desktop/oop\shw3/my_result");
+        MovieQuery.queryAll("imdb3", "my_result");
     }
 }
